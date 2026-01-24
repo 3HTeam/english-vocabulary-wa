44bace9226fa33a089b56477dbbeeda6ed8dc302
dinhminhhieu <dinhminhhieuvn@gmail.com>
2026-01-24 09:46:35 +0700

    update/update-docker-compose-no-lint

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,18 @@
 import { notFound } from "next/navigation";
+
 import { hasLocale, NextIntlClientProvider } from "next-intl";
+
 import "@/app/globals.css";
+
 import { getMessages } from "next-intl/server";
 
+import { publicSans } from "@/assets/styles";
 import {
-  ThemeProvider,
   QueryClientProviderWrapper,
   SidebarConfigProvider,
+  ThemeProvider,
 } from "@/components/shared/providers";
 import { Toaster } from "@/components/ui/sonner";
-import { publicSans } from "@/assets/styles";
 import { routing } from "@/i18n/routing";
 
 export default async function RootLayout({
@@ -17,10 +20,12 @@ export default async function RootLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 }) {
   // Ensure that the incoming `locale` is valid
   const { locale } = await params;
+
+  // Kiểm tra locale hợp lệ
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
